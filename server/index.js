07e9babdb6a747a866eb333e1f7d99bf02adb733
IshanshK <ishanshkhare.in@gmail.com>
2024-06-24 04:43:40 +0530

    User Data Type Definition

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 //basic congigurations
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.post("/auth/register", upload.single("picture"), register);
+
+//mongoose setup
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
